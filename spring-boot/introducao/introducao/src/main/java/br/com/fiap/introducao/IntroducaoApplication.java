b71386ff610ad5854317df56d0a87753406b2abd
diandrade <contato.andradediego@gmail.com>
2026-03-23 22:43:37 -0300

    feat: Início - Spring.

---
+++ spring-boot/introducao/introducao/src/main/java/br/com/fiap/introducao/IntroducaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.introducao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroducaoApplication.class, args);
+	}
+
+}
